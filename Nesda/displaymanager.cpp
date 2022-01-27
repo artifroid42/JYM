3282e253f6acaa6dc0927156d79d9e6ca2b4392f
Hika-li <sabri.crs@gmail.com>
2022-01-27 09:59:03 +0100

    [Add] Affichage

--- Nesda/displaymanager.cpp
+++ Nesda/displaymanager.cpp
@@ -1,6 +1,107 @@
 #include "displaymanager.h"
+#include <GL/glu.h>
 
-DisplayManager::DisplayManager()
+#include "QDebug"
+#include "QDir"
+#include <iostream>
+#include <filesystem>
+#include "fstream"
+
+
+DisplayManager::DisplayManager(QWidget *parent) : QGLWidget(parent), _X(0), _Y(0), _Z(-10)
 {
+    // Update the scene
+    connect( &_timer, SIGNAL(timeout()), this, SLOT(updateGL()));
+    _timer.start(16); // Starts or restarts the timer with a timeout interval of 16 milliseconds.
+}
 
+void DisplayManager::initializeGL()
+{
+    // background color
+    glClearColor(0.07, 0.04, 0.02, 1);
+
+    // Shader
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+
+    fprintf(stderr, "initialisation du Gl\n");
+}
+
+void DisplayManager::paintGL(){
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Center the camera
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glTranslatef(0.0, 0.0, _Z);
+
+    // Rotation
+    //glRotatef(0.f, 90.0f, 0.0, 0.0f);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1, 0 ,0);
+        glVertex3f(-0.5,0.8,0);
+        glVertex3f(0,1.6,0);
+        glVertex3f(0.5,0.8,0);
+
+        glColor3f(0, 0 ,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0.5,0.8,0);
+        glVertex3f(1,0,0);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+        glColor3f(0, 1 ,0);
+        glVertex3f(-1,0,0);
+        glVertex3f(-0.5,0.8f,0);
+        glVertex3f(0,0,0);
+        glVertex3f(-1,0,0);
+    glEnd();
+}
+
+void DisplayManager::resizeGL(int width, int height){
+
+    glViewport(0, 0, width, height); //Viewport in the world window
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, 0.1f, 3000.0f);
+
+    updateGL();
+}
+
+// - - - - - - - - - - - - Mouse Management  - - - - - - - - - - - - - - - -
+// When you click, the position of your mouse is saved
+void DisplayManager::mousePressEvent(QMouseEvent *event)
+{
+    if( event != NULL ) {
+        _lastPosMouse = event->pos();
+        // Do stuff
+        updateGL();
+    }
+}
+
+// Mouse movement management
+void DisplayManager::mouseMoveEvent(QMouseEvent *event)
+{
+    int dx = event->x() - _lastPosMouse.x();
+    int dy = event->y() - _lastPosMouse.y();
+
+    if( event != NULL )
+    {
+        // Do stuff
+        updateGL();
+    }
+}
+
+// Mouse Management for the zoom (for instance)
+void DisplayManager::wheelEvent(QWheelEvent *event) {
+    QPoint numDegrees = event->angleDelta();
+    double stepZoom = 0.25;
+    if (!numDegrees.isNull())
+    {
+      _Z = (numDegrees.x() > 0 || numDegrees.y() > 0) ? _Z + stepZoom : _Z - stepZoom;
+    }
 }
